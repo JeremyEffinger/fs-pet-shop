4e4f05af50cad47902a71635803bd68c513cc85b
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-12-01 13:52:40 -0600

    read and create done.

--- pets.js
+++ pets.js
@@ -1,22 +1,41 @@
-import { readFile } from "node:fs/promises";
+#!/usr/bin/env node
 
-//Usage?: node pets.js [ read | create | update | destroy ]
+import { write } from "fs";
+import { readFile, writeFile } from "fs/promises";
 
 const subcommand = process.argv[2];
+const petIndex = process.argv[3];
 
-if (subcommand === "read") {
-  const petIndex = process.argv[3];
-
-  readFile("./pets.json", "utf-8").then((text) => {
-    const pets = JSON.parse(text);
-    if (petIndex === undefined) {
-      console.log(pets);
-    } else {
-      console.log(pets[petIndex]);
-    }
-  });
-} else if (subcommand === "create") {
-} else {
-  console.error("Usage: node pets.js [read | create | update | destroy]");
-  process.exit(1);
+switch (subcommand) {
+  case "read":
+    readFile("./pets.json", "utf-8").then((text) => {
+      const pets = JSON.parse(text);
+      if (petIndex === undefined) {
+        console.log(pets);
+      } else if (petIndex < 0 || petIndex >= pets.length) {
+        console.error("Usage: node pets.js read INDEX");
+        process.exit(1);
+      } else {
+        console.log(pets[petIndex]);
+      }
+    });
+    break;
+  case "create":
+    readFile("./pets.json", "utf-8").then((text) => {
+      const pets = JSON.parse(text);
+      if (process.argv.length === 6) {
+        const age = parseInt(process.argv[3]);
+        const kind = process.argv[4];
+        const name = process.argv[5];
+        const newAnimal = { age, kind, name };
+        pets[pets.length] = newAnimal;
+        writeFile("./pets.json", JSON.stringify(pets));
+      } else {
+        console.error("Usage: node pets.js create AGE KIND NAME");
+        process.exit(1);
+      }
+    });
+    break;
+  case "update":
+    null;
 }
