53b80fa1267c3578f07e6ee44fe0a19029bde4b5
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-12-02 16:26:12 -0600

    Error checking for required keys.

--- expressServer.js
+++ expressServer.js
@@ -4,6 +4,8 @@ import { readFile, writeFile } from "fs/promises";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
@@ -28,6 +30,29 @@ app.get("/pets/:index", (req, res) => {
       res.set("Content-Type", "text/plain").sendStatus(404);
     }
   });
+});
+
+app.post("/pets", function (req, res) {
+  readFile("./pets.json", "utf-8").then((text) => {
+    const pets = JSON.parse(text);
+    const requiredKeys = ["age", "kind", "name"];
+    if (
+      typeof req.body.age === "number" &&
+      requiredKeys.every((key) => req.body.hasOwnProperty(key))
+    ) {
+      pets.push(req.body);
+      writeFile("./pets.json", JSON.stringify(pets));
+      res.status(201);
+      res.json(pets);
+    } else {
+      res.status(400).send("Bad Request");
+      console.log(req.body);
+    }
+  });
+});
+
+app.use((req, res, next) => {
+  res.contentType("text/plain").status(404).send("Not Found");
 });
 
 app.listen(port, () => {
